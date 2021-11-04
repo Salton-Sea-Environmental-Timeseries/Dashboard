02edb3f4a93f2f2ed39e99fa9d1e163afc5f6069
Salton-Sea-Environmental-Timeseries <57472090+Salton-Sea-Environmental-Timeseries@users.noreply.github.com>
2021-11-03 18:22:54 -0700

    Update app.R

--- dashboard/app.R
+++ dashboard/app.R
@@ -5,6 +5,9 @@ library(ggmap)
 library(ggrepel)
 library(RColorBrewer)
 
+#Run data_cleaner
+suppressWarnings(source("data_cleaner.R"))
+
 #position of site labels
 nudge <- c(rep(0.03,4),0,0,0,0.03,0.03,0.01, 0.03)
 #Set your API Key to call map from google
@@ -12,6 +15,16 @@ ggmap::register_google(key = "AIzaSyA-US0xjpp6-mru0kgOHDSF2eUOHgGsJGI")
 
 # Load data ---------------------------------------------------------
 load("data/data.Rdata")
+
+
+parameters <- c("sal", "temp", "pH", "turbidity", "ODO_mgl", "Chl", "pe", "Nitrate", "Ammonia", "Phosphate", "Sulphate", "Sulphide")
+names(parameters) <- c("Salinity", "Water Temperature", "pH", "Turbidity", "Dissolved Oxygen", "Chlorophyll", "Phycoerythrin", "Nitrate", "Ammonia", "Phosphate", "Sulphate", "Sulphide")
+
+titles <- c("Salinity", "Water Temperature", "pH", "Turbidity", "Dissolved Oxygen", "Chlorophyll", "Phycoerythrin", "Nitrate", "Ammonia", "Phosphate", "Sulphate", "Sulphide")
+names(titles) <- parameters
+
+units <- c("PSU", "°C", "", "FNU", "mg/L", rep("µg/L",2), rep("mg/L",5))
+names(units) <- parameters
 
 
 # Define UI ---------------------------------------------------------
@@ -23,11 +36,17 @@ ui <- fluidPage(
         # Inputs: Select variables to plot
         sidebarPanel(
             
-            # Select variable for y-axis
-            selectInput(inputId = "param", 
-                        label = "Parameter:",
+            # Select variable 
+            selectInput(inputId = "param1", 
+                        label = "Parameter 1:",
                         choices = parameters, 
                         selected = "Chl"),
+            
+            # Select variable 
+            selectInput(inputId = "param2", 
+                        label = "Parameter 2:",
+                        choices = parameters, 
+                        selected = "Nitrate"),
             
             # Show data table
             checkboxInput(inputId = "show_data",
@@ -41,11 +60,16 @@ ui <- fluidPage(
             downloadButton("downloadNUTS", "Download Nutrient")
         ),
         
+        
+        
         # Output
         mainPanel(
             
-            # Show mapplot
-            plotOutput(outputId = "mapplot"),
+            # Show mapplot1
+            plotOutput(outputId = "mapplot1"),
+            
+            # Show mapplot2
+            plotOutput(outputId = "mapplot2"),
             
             # Show data table
             DT::dataTableOutput(outputId = "sstable")
@@ -57,21 +81,42 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     
-    # Create mapplot object the plotOutput function is expecting
-    output$mapplot <- renderPlot({
+    # Create mapplot1 object the plotOutput function is expecting
+    output$mapplot1 <- renderPlot({
         
 
-        myPalette <- colorRampPalette(brewer.pal(9, colors[1]), space="Lab") #set unique color palatte for color gradient
+        myPalette <- colorRampPalette(brewer.pal(9, "YlGnBu"), space="Lab") #set unique color palatte for color gradient
         
         p <- qmap(c(lon = -116.048047, lat =  33.47), zoom = 13, source = "google", maptype = "terrain", crop = FALSE) +
             geom_label_repel(data = d.all, aes(x = Longitude, y = Latitude, label = Station), colour = "black", 
                              force = 1, size = 4.5, box.padding = 2, nudge_x = nudge,
                              min.segment.length = 1, segment.size = 1.1, segment.color = "darkgrey") +
-            geom_point(data = d.all, aes_string(x = "Longitude", y = "Latitude", color = input$param), alpha=1, stroke = 4, size = 4, na.rm = T) +
+            geom_point(data = d.all, aes_string(x = "Longitude", y = "Latitude", color = input$param1), alpha=1, stroke = 4, size = 4, na.rm = T) +
             scale_colour_gradientn(colours = myPalette(100),
                                    guide = "colourbar",
-                                   name = units[names(units) ==  input$param]) + 
-            ggtitle(titles[names(titles) == input$param])+
+                                   name = units[names(units) ==  input$param1]) + 
+            ggtitle(titles[names(titles) == input$param1])+
+            theme(plot.title = element_text(size = 20, face = "bold"),
+                  legend.position="bottom", legend.box = "horizontal")
+        
+        suppressWarnings(print(p))
+    })
+    
+    # Create mapplot2 object the plotOutput function is expecting
+    output$mapplot2 <- renderPlot({
+        
+        
+        myPalette <- colorRampPalette(brewer.pal(9, "YlOrRd"), space="Lab") #set unique color palatte for color gradient
+        
+        p <- qmap(c(lon = -116.048047, lat =  33.47), zoom = 13, source = "google", maptype = "terrain", crop = FALSE) +
+            geom_label_repel(data = d.all, aes(x = Longitude, y = Latitude, label = Station), colour = "black", 
+                             force = 1, size = 4.5, box.padding = 2, nudge_x = nudge,
+                             min.segment.length = 1, segment.size = 1.1, segment.color = "darkgrey") +
+            geom_point(data = d.all, aes_string(x = "Longitude", y = "Latitude", color = input$param2), alpha=1, stroke = 4, size = 4, na.rm = T) +
+            scale_colour_gradientn(colours = myPalette(100),
+                                   guide = "colourbar",
+                                   name = units[names(units) ==  input$param2]) + 
+            ggtitle(titles[names(titles) == input$param2])+
             theme(plot.title = element_text(size = 20, face = "bold"),
                   legend.position="bottom", legend.box = "horizontal")
         
